3ffcb483281a78c2a8e85ad2576788de969b79f4
Hasara <hasarasamson97@gmail.com>
2021-07-12 11:50:24 +0530

    Setting up hibernate

--- src/main/java/com/inventory/demo/InventoryManagementApiApplication.java
+++ src/main/java/com/inventory/demo/InventoryManagementApiApplication.java
@@ -2,9 +2,8 @@ package com.inventory.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class InventoryManagementApiApplication {
 
     public static void main(String[] args) {
